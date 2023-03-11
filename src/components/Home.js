38e305566808131cd43867a6a4db1d12fa0f87d0
ridipblog <83365493+ridipblog@users.noreply.github.com>
2023-03-10 23:47:18 -0800

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 const Home = () => {
     const navigate = useNavigate();
-    const [userData, setUserData] = useState('');
+    const [userData, setUserData] = useState({});
     useEffect(() => {
         const callHomePage = async () => {
            try {
@@ -17,7 +17,7 @@ const Home = () => {
                const res=await fetch('https://mernback-jma2.onrender.com/home');
                 const data=await res.json();
                 console.log(data.name)
-                setUserData(data.name);
+                setUserData(data);
             } catch (err) {
                 console.log(err);
                 navigate('/login')
@@ -29,7 +29,7 @@ const Home = () => {
         <>
             <div>
                 <form method='GET'>
-                    <p>welcome {userData}  </p>
+                    <p>welcome {userData.name}  </p>
                     <h1 className='heading'>We Are Mern Developer </h1>
                 </form>
             </div>
