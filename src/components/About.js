2a2f280ca977b8f07c684ad6162ec607ce00d589
ridipblog <83365493+ridipblog@users.noreply.github.com>
2023-03-11 09:15:00 -0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
 const About = () => {
     const navigate = useNavigate();
     const [userData, setUserData] = useState({});
+    const [cookies, setCookie] = useCookies(['user']);
     useEffect(() => {
         const callAboutPage = async () => {
             try {
@@ -15,7 +17,7 @@ const About = () => {
 //                 });
                 const res=await fetch('https://mernback-jma2.onrender.com/about');
                 const data = await res.json();
-                console.log(data);
+                console.log(cookies.Name);
                 setUserData(data);
                 if (!res.status === 200) {
                     const error = new Error(res.error);
@@ -35,6 +37,7 @@ const About = () => {
                     <div className="card text-center">
                         <img src="..." className="card-img-top" alt="Card" />
                         <div className="card-body">
+                            <h1>Cookie Name : {cookies.Name}</h1>
                             <h5 className="card-title">{userData.name}</h5>
                             <p className="card-text">{userData.email}</p>
                             <p className="card-text">{userData.phone}</p>
